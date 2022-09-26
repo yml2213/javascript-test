85a83b53ca04969d7a3d5c768e0827c4f723df0e
yml2213 <a2213290201@gmail.com>
2022-09-26 19:46:18 +0800

    111

---
+++ 盘子/test.js
@@ -0,0 +1,17 @@
+
+method = 'Get'
+let get_arr = ['get', 'Get', 'GET']
+let post_arr = ['post', 'Post', 'POST']
+
+
+if (get_arr.indexOf(method) > -1) {
+	console.log(`==================444======`);
+
+
+} else if (post_arr.indexOf(method) > -1) {
+	console.log(`==================222======`);
+
+
+} else {
+	console.log(`未知请求类型，请自行排查！`);
+}
