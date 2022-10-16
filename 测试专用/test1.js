94de997113f90d919cca090d7bd9a1fd267192f8
yml2213 <a2213290201@gmail.com>
2022-10-16 17:16:01 +0800

    11

--- 测试专用/test1.js
+++ 测试专用/test1.js
@@ -1,34 +1,42 @@
-var request = require('request');
-var options = {
-  'method': 'POST',
-  'url': 'https://wxa-tp.ezrpro.com/myvip/Vip/SignIn/SignIn',
-  'headers': {
-    'Host': 'wxa-tp.ezrpro.com',
-    'Connection': 'keep-alive',
-    'Content-Length': '36',
-    'ezr-cop-id': '143',
-    'charset': 'utf-8',
-    'ezr-vuid': '83502527',
-    'uber-trace-id': '53a3a62c359165a7:53a3a62c359165a7:0:1}',
-    'ezr-sp': '2',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 12; 22041211AC Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3171 MMWEBSDK/20211202 Mobile Safari/537.36 MMWEBID/3690 MicroMessenger/8.0.18.2061(0x28001240) Process/appbrand0 WeChat/arm64 Weixin GPVersion/1 NetType/WIFI Language/zh_CN ABI/arm64 MiniProgramEnv/android',
-    'Accept-Encoding': 'gzip,compress,br,deflate',
-    'ezr-source': 'weapp',
-    'ezr-st': '1657245283381',
-    'ezr-ss': '67d772578b8fe5fde7deb0b3a0d70e67781990c2',
-    'ezr-userid': 'oMfWKs09zDDiGdi_wDfGrC_mAGgw',
-    'ezr-sv': '1',
-    'ezr-brand-id': '254',
-    'content-type': 'application/json',
-    'Referer': 'https://servicewechat.com/wx12e1cb3b09a0e6f0/72/page-frame.html'
-  },
-  body: JSON.stringify({
-    "ActId": 784,
-    "ActRemindStatus": true
-  })
 
-};
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
+var a = n("1da1")
+  , c = n("5530")
+  , i = (n("96cf"),
+    n("d3b7"),
+    n("25f0"),
+    n("d81d"),
+    n("4e82"),
+    n("b64b"),
+    n("6821"))
+  , o = n.n(i)
+  , r = n("bc3a")
+  , u = n.n(r)
+  , s = n("4360")
+  , l = n("d399")
+  , d = n("a18c")
+  , p = n("f524")
+  , h = n("4328")
+
+function md5(e) {
+  for (var t = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", n = "", a = 0; a < 8; a++)
+    n += t.substr(Math.floor(Math.random() * t.length), 1);
+  var i = Object(c["a"])(Object(c["a"])({}, e), {}, {
+    appId: "tea",
+    time: Math.round((new Date).getTime() / 1e3).toString(),
+    nonceStr: n
+  })
+    , r = {};
+  Object.keys(i).sort().map((function (e) {
+    r[e] = i[e]
+  }
+  ));
+  var u = ""
+    , s = parseInt(Object.keys(r).length);
+  for (e in r)
+    s--,
+      u += e + "=" + r[e] + (0 === s ? "" : "&");
+  return "?" + u + "&sign=" + o()(u + "e3de382b2bab1232s")
+}
+
+console.log(md5('111'));
+
